49a8866a8443169868e6a10327329f191cbb3c73
AlcantuMurilo77 <alcantumurilo@gmail.com>
2026-07-16 06:42:34 -0400

    mongo connection up and running with docker

---
+++ internal/database/mongo.go
@@ -0,0 +1,28 @@
+package database
+
+import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+
+func Connect(uri string) (*mongo.Client, error){
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	clientOptions := options.Client().ApplyURI(uri)
+
+	client, err := mongo.Connect(clientOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := client.Ping(ctx, nil); err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
--- main.go
+++ main.go
@@ -1,23 +1,39 @@
 package main
 
 import(
+	"context"
 	"net/http"
 	"log"
+	"time"
+	"os"
+	"github.com/AlcantuMurilo77/tic/internal/database"
+	"github.com/joho/godotenv"
 )
 
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Erro ao carregar .env")
+	}
+	uri := os.Getenv("MONGODB_URI")
+
+	client, err := database.Connect(uri)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func(){
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		if err := client.Disconnect(ctx); err != nil {
+			log.Println(err)
+		}
+	}()
+
 	log.Println("Server running on :8080")
-	game := NewTicTacToe(3)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 
-	game.Move(0,0, 1)
-	game.Move(1,0,1)
-	game.Move(2, 0, 1)
-	result := game.Move(2, 0, 2)
-
-
-	log.Println(game.Rows)
-	log.Println(game.Cols)
-	log.Println(result)
-	http.ListenAndServe(":8080", nil)
 }
